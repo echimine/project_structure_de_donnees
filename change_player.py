40364617c7c4550d3b432b48ff2a62814a614d84
clementAvesque <clement2avesque@gmail.com>
2025-10-14 15:36:21 +0200

    ajout de la creation du premier compte

--- change_player.py
+++ change_player.py
@@ -18,30 +18,42 @@ def createPlayer(Name):
     arbre.write(url, encoding="utf-8", xml_declaration=True)
 
 
+#fonction qui permet de commencer une parti 
 def startGame():
     import os
     print("salut salut , tu viens de lancer notre JDR sur l'univers de Stephen king")
-    print("tu as un compte => 1")
-    print("crée en toi un =>2")
-    choice = input("choix:")
-    match choice:
-        case "1":
-            data = os.listdir("player")
-            if len(data) == 1 :
-                return data[0]
-            else :
-                print("choisi ton compte:")
-                for index,file in enumerate(data):
-                    print(data[index]+"=>"+str(index))
-                file = input("quel est ton compte?:")
-                while 0>int(file) or len(data)<int(file):
+
+    data = os.listdir("player")
+    if len(data)>0:
+        print("tu as un compte => 1")
+        print("crée en toi un =>2")
+        choice = input("choix:")
+        match choice:
+            case "1":
+                #retourne la parti si il n'y en a qu'une
+
+                if len(data) == 1 :
+                    return data[0]
+                #si il y en a plusieur faire une liste pour que l'utilisateur puisse choisir
+                else :
+                    print("choisi ton compte: ")
+                    for index,file in enumerate(data):
+                        print(data[index]+" => "+str(index))
+                    file = input("quel est ton compte?: ")
+                    #tant que le joueur met une valeur interdite alors lui poser la question
+                    while 0>int(file) or len(data)<int(file):
                         print("tu t'es trompé, choisi une valeur mise")
-                        file = input ("choix:")
-                return data[int(file)]         
-        case "2":
-            name = input("choisi ton nom:")
-            createPlayer(name)
-            return name+".xml"
+                        file = input ("choix: ")
+                    return data[int(file)]  
+                #si le joueur n'a pas de parti en cours il peut la crée
+            case "2":
+                name = input("choisi ton nom: ")
+                createPlayer(name)
+                return name+".xml"
+    else:
+        name = input("choisi ton nom: ")
+        createPlayer(name)
+        return name+".xml"
 
 
 
